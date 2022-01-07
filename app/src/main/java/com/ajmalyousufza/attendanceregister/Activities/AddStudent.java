0cbed24ac938dafbe325f8097e097d2906720755
Ajmalyousufza <ajmalyousufza@gmail.com>
2022-01-07 23:10:26 +0530

    Add Student Activity set up finished and connected on firebase

--- app/src/main/java/com/ajmalyousufza/attendanceregister/Activities/AddStudent.java
+++ app/src/main/java/com/ajmalyousufza/attendanceregister/Activities/AddStudent.java
@@ -1,16 +1,80 @@
 package com.ajmalyousufza.attendanceregister.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ajmalyousufza.attendanceregister.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class AddStudent extends AppCompatActivity {
+
+
+
+    EditText studentClass;
+    EditText studentName;
+    Button addStudent;
+    FirebaseAuth auth;
+    FirebaseFirestore firestore;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+
+        studentClass = findViewById(R.id.student_class_added);
+        studentName = findViewById(R.id.studentName_added);
+        addStudent = findViewById(R.id.add_student_button);
+        progressBar = findViewById(R.id.prgressbarr);
+        progressBar.setVisibility(View.GONE);
+
+        auth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+
+        addStudent.setOnClickListener(view -> {
+            progressBar.setVisibility(View.VISIBLE);
+            String stdName = studentName.getText().toString();
+            String stdClass = studentClass.getText().toString();
+            //String currentMonth;
+
+//            DateFormat dateFormat = new SimpleDateFormat("MM");
+//            Date date = new Date();
+//            Log.d("Month",dateFormat.format(date));
+//            currentMonth = dateFormat.format(date);
+
+            String uid = Objects.requireNonNull(auth.getCurrentUser()).getUid();
+            HashMap<String,Object> stdatalist = new HashMap<>();
+            stdatalist.put("name",stdName);
+            stdatalist.put("class",stdClass);
+
+            firestore.collection("Admin").document(uid)
+                    .collection("StudentDetailes").document(stdClass)
+                    .set(stdatalist).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(AddStudent.this, "Student "+stdName+" added", Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+
     }
 }
--- app/src/main/java/com/ajmalyousufza/attendanceregister/Activities/MainActivity.java
+++ app/src/main/java/com/ajmalyousufza/attendanceregister/Activities/MainActivity.java
@@ -2,15 +2,30 @@ package com.ajmalyousufza.attendanceregister.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.ajmalyousufza.attendanceregister.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText schoolname;
+    Button schoolupdate,add_student;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        schoolname = findViewById(R.id.schoolName);
+        schoolupdate = findViewById(R.id.schoolNameUpdate_btn);
+        add_student = findViewById(R.id.add_student_btn);
+
+        add_student.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this,AddStudent.class));
+        });
+
     }
 }
